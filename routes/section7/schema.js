4c56b70ae8d9f1276936d25a0f605580ac649fb4
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-07 16:40:32 -0500

    add section 7 form

--- routes/section7/schema.js
+++ routes/section7/schema.js
@@ -1,9 +1,8 @@
+/* istanbul ignore file */
+
 const Schema = {
-  firstname: {
-    isLength: {
-      errorMessage: 'errors.firstname.length',
-      options: { min: 3, max: 200 },
-    },
+  other_info: {
+    
   },
 }
 
