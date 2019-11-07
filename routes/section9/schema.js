3d69b112d2daa6eda9469e82403fff319d5e68f2
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-07 17:21:19 -0500

    build section 9 form and validation

--- routes/section9/schema.js
+++ routes/section9/schema.js
@@ -1,8 +1,56 @@
+/* istanbul ignore file */
+
+const isValidDate = require('../../utils/custom/validate.helpers.js').isValidDate
+const validateDateFormat = require('../../utils/custom/validate.helpers.js').validateDateFormat
+
 const Schema = {
-  firstname: {
+  i_am_a: {
+    isIn: {
+      errorMessage: 'I am a is required',
+      options: [['1', '2', '3', 'other']],
+    },
+  },
+  name: {
     isLength: {
-      errorMessage: 'errors.firstname.length',
-      options: { min: 3, max: 200 },
+      errorMessage: 'Name is required',
+      options: { min: 1 },
+    },
+  },
+  date: {
+    customSanitizer: {
+      options: value => {
+        // We want to remove any spaces, dash or underscores
+        return value ? value.replace(/[_]*/g, '') : value
+      },
+      errorMessage: 'Date format is incorrect',
+    },
+    custom: {
+      options: (value, { req }) => {
+        if (!value) {
+          this.message = 'Date is required'
+          return false
+        }
+
+        if (!validateDateFormat(value)) {
+          this.message = 'Date is incorrectly formatted'
+          return false
+        } 
+        
+        if (!isValidDate(value)) {
+          this.message = 'Date is not a valid date'
+          return false
+        }
+        return true;
+      },
+      errorMessage: () => {
+        return this.message
+      },
+    },
+  },
+  address: {
+    isLength: {
+      errorMessage: 'Address is required',
+      options: { min: 1 },
     },
   },
 }
