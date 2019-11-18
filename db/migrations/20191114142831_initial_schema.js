ad0c0798d495ea35bd4960c0cfbf9795cb8ff408
Calvin Rodo <calvinv@gmail.com>
2019-11-18 14:00:05 -0500

    working on schema

--- db/migrations/20191114142831_initial_schema.js
+++ db/migrations/20191114142831_initial_schema.js
@@ -2,20 +2,74 @@
 exports.up = function(knex) {
  return knex.schema.createTable('medical-report', t => { 
     t.increments();
+    // Section 1
     t.string('SIN', 9).notNullable();
-    t.string('Title',4);
-    t.string('FirstName',40).notNullable();
-    t.string('MiddleName',40);
-    t.string('LastName',40).notNullable();
-    t.string('LastNameAtBirth',40);
+    t.string('Title');
+    t.string('FirstName').notNullable();
+    t.string('MiddleName');
+    t.string('LastName').notNullable();
+    t.string('LastNameAtBirth');
     t.date('BirthDate').notNullable();
-    t.string('Address',255);
-    t.string('TelephoneNumber', 40).notNullable();
-    t.string('AltTelephoneNumber', 40);
+    t.string('Address');
+    t.string('TelephoneNumber').notNullable();
+    t.string('AltTelephoneNumber');
     t.enu('ContactPeriod',['am','pm','never'], {useNative: false, enumName: 'ContactPeriod'}).notNullable();
- });
+
+    // Section 2
+    t.enu('Consent', ['yes', 'no'], {useNative: false, enumName: 'Consent'}).notNullable();
+    // XXX: Storing this as a string right, will either be a blob or a link to a file in cloud
+    t.string('Signature');
+    t.string('WitnessFirstName');
+    t.string('WitnessMiddleName');
+    t.string('WitnessLastName');
+    t.string('WitnessPhoneNumber');
+    t.string('WitnessSignature');
+
+    // Section 3
+    t.enu('YearsInCare', ['<1','1-2','3-4','5>'], {useNative: false, enumName: 'YearsInCare'}).notNullable();
+    t.integer('NumberOfVisitsInLastYear').notNullable();
+    t.date('DateOfLastVisit').notNullable();
+    t.date('DateOfFirstTreatmentForPrimaryCondition').notNullable();
+  
+    // Section 4
+    t.enu('ConditionState', ['grave', 'terminal', 'neither'], {useNative: false, enuName: 'ConditionState'}).notNullable();
+    t.string('TerminalCondition');
+    t.string('ICD-9M-Code');
+    t.date('DateOfSymptonOnset');
+
+ })
+ .createTable('medical-condition', t => {
+    t.increments();
+    t.string('ConditionName').notNullable();
+    t.string('ICD-9-CM').notNullable();
+    t.string('DateOfSymtponOnset').notNullable();
+    t.string('Impairments').notNullable();
+    t.string('FunctionalLimitations').notNullable();
+    t.enu('Prognosis', ['improve', 'deteriorate', 'same', 'unknown'], {useNative: false, enuName: 'Prognosis'}).notNullable();
+    t.enu('ExpectedDuration',['<1', '1>'], {useNative:false, enuName: 'ExpectedDuration'} ).notNullable();
+    t.enu('Frequency', ['recurrant', 'continuous', 'unknown'], {useNative:false, enuName: 'Frequency'} ).notNullable();
+    t.integer('reportId').unsigned().notNullable();
+    t.foreign('reportId').references('id').inTable('medical-report').onDelete('cascade')
+
+  }).createTable('medication', t => { 
+      t.increments();
+      t.string('medicationInfo').notNullable();
+      t.date('startDate');
+      t.date('endDate');
+      t.string('response');
+  }).createTable('treatment', t => {
+    t.increments();
+    t.string('treatmentInfo'); 
+    t.date('startDate');
+    t.date('endDate');
+    t.string('response');
+  });
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTable('medical-report');
+    return knex.schema
+                .dropTable('treatment')
+                .dropTable('medication')
+                .dropTable('medical-condition')
+                .dropTable('medical-report');
 };
