c207fa2447970186cc88a7c737a634d9252a28dd
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-19 10:55:24 -0500

    render vue templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = (env, argv) => {
   const { getConfig } = require('@cdssnc/webpack-starter')
@@ -6,6 +7,7 @@ module.exports = (env, argv) => {
     mode: argv.mode,
     entry: {
       styles: './assets/scss/app.scss',
+      app: './assets/js/app.js',
       personal: './routes/personal/js/personal.js',
     },
     output: {
@@ -13,6 +15,36 @@ module.exports = (env, argv) => {
       path: path.resolve(__dirname, 'public/dist'),
     },
     stats: 'errors-only',
+    module: {
+      rules: [
+        // ... other rules
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+        // this will apply to both plain `.js` files
+        // AND `<script>` blocks in `.vue` files
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+        },
+        // this will apply to both plain `.css` files
+        // AND `<style>` blocks in `.vue` files
+        {
+          test: /\.css$/,
+          use: ['vue-style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      // make sure to include the plugin!
+      new VueLoaderPlugin(),
+    ],
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.esm.js', // Use the full build
+      },
+    },
   })
 
   return config
