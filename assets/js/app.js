b069ec55daf0900bfe7fdd863b17806fa9cd71b8
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-19 10:55:44 -0500

    add app-level js/vue components

---
+++ assets/js/app.js
@@ -0,0 +1,62 @@
+import Vue from 'vue'
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap')
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+window.Event = new (class {
+  constructor() {
+    this.vue = new Vue()
+  }
+
+  fire(event, data) {
+    this.vue.$emit(event, data)
+  }
+
+  listen(event, callback) {
+    this.vue.$on(event, callback)
+  }
+})()
+
+// var VueScrollTo = require('vue-scrollto')
+
+Vue.prototype.$last = function(item, list) {
+  return item === list[list.length - 1]
+}
+
+Vue.prototype.$first = function(item, list) {
+  return item === list[0]
+}
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key =>
+  Vue.component(
+    key
+      .split('/')
+      .pop()
+      .split('.')[0],
+    files(key).default,
+  ),
+)
+
+const app = new Vue({
+  el: '#app',
+})
