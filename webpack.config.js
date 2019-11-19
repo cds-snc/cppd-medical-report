c90dc1f4376e8376235ed98ffa1a99b4a01a9d3e
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-19 15:47:25 -0500

    simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -22,18 +22,6 @@ module.exports = (env, argv) => {
           test: /\.vue$/,
           loader: 'vue-loader',
         },
-        // this will apply to both plain `.js` files
-        // AND `<script>` blocks in `.vue` files
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-        },
-        // this will apply to both plain `.css` files
-        // AND `<style>` blocks in `.vue` files
-        {
-          test: /\.css$/,
-          use: ['vue-style-loader', 'css-loader'],
-        },
       ],
     },
     plugins: [
