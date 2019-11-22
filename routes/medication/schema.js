8ec73da7e32dd72a3438175099d2f8d89168e3bc
Calvin Rodo <calvinv@gmail.com>
2019-11-22 11:25:34 -0500

    Medication first pass (#46)
    * Mocking up medication page
    
    * Adding text
    
    * Adding text and validations
    
    * Removing unused keys
    
    * Fixing up text
    
    * Fix error with firstname on medication page
    
    Fix error message keys
    Add keys to french page.

---
+++ routes/medication/schema.js
@@ -0,0 +1,55 @@
+/* istanbul ignore file */
+const Schema = {
+  medication_name: { 
+    isLength: { 
+      errorMessage: 'errors.medication_name.length',
+      options: { min: 1 },
+    },
+  },
+  medication_dosage: {
+    isLength: { 
+      errorMessage: 'errors.medication_dosage.length',
+      options: { min: 1 },
+    },
+  },
+  medication_frequency: {
+    isLength: { 
+      errorMessage: 'errors.medication_frequency.length',
+      options: { min: 1 },
+    },
+  },
+  medication_start_date : {
+    isLength: { 
+      errorMessage: 'errors.medication_start_date.length',
+      options: { min: 1 },
+    },
+  },
+  medication_end_date: { 
+    isLength: { 
+      errorMessage: 'errors.medication_end_date.length',
+      options: { min: 1 },
+    },
+  },
+  medication_treated_condition: {
+    custom: {
+      options: (value, { req }) => {
+        console.log(JSON.stringify(value))
+        if(value === undefined || value.length === 0){
+          return false;
+        }
+        return true;
+      },
+      errorMessage: 'errors.medication_treated_condition.is_checked',
+    },
+  },
+  medication_results: {
+    isLength: { 
+      errorMessage: 'errors.medication_results.length',
+      options: { min: 1 },
+    },
+  },
+}
+
+module.exports = {
+  Schema,
+}
