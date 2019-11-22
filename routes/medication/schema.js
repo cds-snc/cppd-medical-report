8eff77f73c2b719e6e78fb696dd7acb3c0daae47
Calvin Rodo <calvinv@gmail.com>
2019-11-21 19:53:10 -0500

    Adding text and validations

--- routes/medication/schema.js
+++ routes/medication/schema.js
@@ -1,10 +1,57 @@
 /* istanbul ignore file */
-
 const Schema = {
   firstname: {
     isLength: {
       errorMessage: 'errors.firstname.length',
       options: { min: 3, max: 200 },
+    },
+  },
+  medication_name: { 
+    isLength: { 
+      errorMessage: 'errors.medication_name.length',
+      options: { min: 1 },
+    },
+  },
+  medication_dosage: {
+    isLength: { 
+      errorMessage: 'errors.medication_name.length',
+      options: { min: 1 },
+    },
+  },
+  medication_frequency: {
+    isLength: { 
+      errorMessage: 'errors.medication_name.length',
+      options: { min: 1 },
+    },
+  },
+  medication_start_date : {
+    isLength: { 
+      errorMessage: 'errors.medication_name.length',
+      options: { min: 1 },
+    },
+  },
+  medication_end_date: { 
+    isLength: { 
+      errorMessage: 'errors.medication_name.length',
+      options: { min: 1 },
+    },
+  },
+  medication_treated_condition: {
+    custom: {
+      options: (value, { req }) => {
+        console.log(JSON.stringify(value))
+        if(value === undefined || value.length === 0){
+          return false;
+        }
+        return true;
+      },
+      errorMessage: 'errors.medication_treated_condition.is_checked',
+    },
+  },
+  medication_results: {
+    isLength: { 
+      errorMessage: 'errors.medication_name.length',
+      options: { min: 1 },
     },
   },
 }
