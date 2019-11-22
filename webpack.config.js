abfd0c2af5324e9dce0cee2875c5cf3c2a87e85b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-22 15:17:10 -0500

    styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const path = require("path");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = (env, argv) => {
-  const { getConfig } = require("@cdssnc/webpack-starter");
+  const { getConfig } = require('@cdssnc/webpack-starter')
   const config = getConfig({
     mode: argv.mode,
     entry: {
-      styles: "./assets/scss/app.scss",
-      app: "./assets/js/app.js",
-      personal: "./routes/personal/js/personal.js"
+      styles: './assets/scss/app.scss',
+      app: './assets/js/app.js',
+      personal: './routes/personal/js/personal.js',
     },
     output: {
-      filename: "js/[name].[chunkhash].js",
-      path: path.resolve(__dirname, "public/dist")
+      filename: 'js/[name].[chunkhash].js',
+      path: path.resolve(__dirname, 'public/dist'),
     },
-    stats: "errors-only",
+    stats: 'errors-only',
     module: {
       rules: [
         // ... other rules
         {
           test: /\.vue$/,
-          loader: "vue-loader"
-        }
-      ]
+          loader: 'vue-loader',
+        },
+      ],
     },
     plugins: [
       // make sure to include the plugin!
-      new VueLoaderPlugin()
+      new VueLoaderPlugin(),
     ],
     resolve: {
       alias: {
-        vue$: "vue/dist/vue.esm.js" // Use the full build
-      }
-    }
-  });
+        vue$: 'vue/dist/vue.esm.js', // Use the full build
+      },
+    },
+  })
 
-  return config;
-};
+  return config
+}
