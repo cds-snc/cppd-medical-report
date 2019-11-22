c89250e0431b3e35654079407c4a1f532227596e
Dave Samojlenko <dsamojlenko@gmail.com>
2019-11-21 23:58:35 -0500

    start stubbing out some vue components

---
+++ assets/js/components/MedicalCondition.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      symptom_onset: '',
+      clinical_impairment: '',
+      condition_outlook: '',
+      condition_outlook_unknown: '',
+      condition_duration: '',
+      symptom_frequency: '',
+      documents: [],
+    }
+  },
+  props: ['conditions'],
+  methods: {},
+  mounted() {
+    console.log('Component ready.')
+  },
+}
+</script>
