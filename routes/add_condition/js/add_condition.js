744ae54058f21478c16111caec211ff0147d9062
Dave Samojlenko <dsamojlenko@gmail.com>
2019-11-22 23:24:04 -0500

    better comparison

--- routes/add_condition/js/add_condition.js
+++ routes/add_condition/js/add_condition.js
@@ -2,11 +2,11 @@
     const unknown = document.getElementById('unknown_details')
     unknown.style.display = 'none'
 
-    var radios = document.forms[0].elements['condition_outlook'];
+    var radios = document.forms[0].elements.condition_outlook;
 
     for (var i = 0, len = radios.length; i < len; i++) {
         radios[i].onclick = function () {
-            if (this.checked && this.value == 4) {
+            if (this.checked && this.value === '4') {
                 unknown.style.display = 'block'
             } else {
                 unknown.style.display = 'none'
