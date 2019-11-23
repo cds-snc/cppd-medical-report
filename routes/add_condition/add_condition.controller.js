8ee62f0a1e573141d63b9096b7ea6ef638d99da5
Dave Samojlenko <dsamojlenko@gmail.com>
2019-11-22 23:20:48 -0500

    remove unused var

--- routes/add_condition/add_condition.controller.js
+++ routes/add_condition/add_condition.controller.js
@@ -3,8 +3,6 @@ const { routeUtils, getSessionData, saveSessionData, getClientJs } = require('./
 const { Schema } = require('./schema.js')
 
 module.exports = (app, route) => {
-  const name = route.name
-
   route
     .draw(app)
     .get((req, res) => {
