326997966c1b8b18840da350cea73acb74171e55
Dave Samojlenko <dsamojlenko@gmail.com>
2019-11-22 21:38:56 -0500

    no validation

--- routes/documents/schema.js
+++ routes/documents/schema.js
@@ -1,12 +1,7 @@
 /* istanbul ignore file */
 
 const Schema = {
-  firstname: {
-    isLength: {
-      errorMessage: 'errors.firstname.length',
-      options: { min: 3, max: 200 },
-    },
-  },
+  //
 }
 
 module.exports = {
