99b9a2f33f5d70a9d2c6b32fb53629fa817443d2
Jeff Maher <jeff.maher@cds-snc.ca>
2019-11-25 15:32:59 -0500

    Fixed a linting issue

--- utils/featureFlags.helpers.js
+++ utils/featureFlags.helpers.js
@@ -1,5 +1,5 @@
 function userResearch20191125() {
-    if(typeof process.env.FLAG_USER_RESEARCH_20191125 != undefined ) {
+    if(typeof process.env.FLAG_USER_RESEARCH_20191125 !== "undefined" ) {
         if(process.env.FLAG_USER_RESEARCH_20191125 === "false") {
             return false
         }
