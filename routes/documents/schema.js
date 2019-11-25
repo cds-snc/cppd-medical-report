da9872659ba3d99197e26b4205b3744f7e49cb02
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-25 11:17:37 -0500

    add validation

--- routes/documents/schema.js
+++ routes/documents/schema.js
@@ -1,7 +1,20 @@
 /* istanbul ignore file */
 
 const Schema = {
-  //
+  supporting_documents: {
+    custom: {
+      options: (value, { req }) => {
+        const attachLater = req.body.attach_later
+        if (typeof attachLater === 'undefined') {
+          if (value === '') {
+            return false
+          }
+        }
+        return true
+      },
+      errorMessage: 'errors.supporting_documents_required',
+    },
+  },
 }
 
 module.exports = {
