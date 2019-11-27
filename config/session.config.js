482ddcf50159b67cd0d154d2e0a0a0c9e4fbb8c0
Calvin Rodo <calvinv@gmail.com>
2019-11-27 12:47:57 -0500

    Added the ability to switch session types
    At the moment you can switch between a memory and a cookie session type

--- config/session.config.js
+++ config/session.config.js
@@ -1,13 +1,21 @@
 const session = require('express-session')
 const MemoryStore = require('memorystore')(session)
+const cookieSession = require('cookie-session')
 
 const oneHour = 1000 * 60 * 60
 const sessionName = `ctb-${process.env.SESSION_SECRET ||
   Math.floor(new Date().getTime() / oneHour)}`
-
-// default setup: https://github.com/roccomuso/memorystore#setup
-// options: https://github.com/expressjs/session#options
-module.exports = session({
+  
+const cookieSessionConfig = {
+  name: sessionName,
+  secret: sessionName,
+  cookie: {
+    httpOnly: true,
+    maxAge: oneHour,
+    sameSite: true,
+  },
+}
+const memorySessionConfig = {
   cookie: { httpOnly: true, maxAge: oneHour, sameSite: 'strict' },
   store: new MemoryStore({
     checkPeriod: oneHour, // prune expired entries every hour
@@ -16,4 +24,15 @@ module.exports = session({
   resave: false,
   saveUninitialized: false,
   unset: 'destroy',
-})
+}
+
+const sessions = { 
+  cookie : cookieSession(cookieSessionConfig),
+  memory: session(memorySessionConfig),
+}
+
+const appSession = sessions.cookie
+
+// default setup: https://github.com/roccomuso/memorystore#setup
+// options: https://github.com/expressjs/session#options
+module.exports = appSession
