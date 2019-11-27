bdf5346e0215ce4872bdf97c03dceb753fb16501
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-27 16:14:04 -0500

    enable condition deletion

---
+++ routes/delete_condition/delete_condition.controller.js
@@ -0,0 +1,12 @@
+const { getSessionData } = require('./../../utils')
+
+module.exports = (app, route) => {
+  route.draw(app).post((req, res) => {
+    const data = getSessionData(req)
+    const index = req.params.id - 1 // array is zero-indexed
+
+    data.conditions.splice(index, 1)
+
+    res.redirect(res.locals.routePath('conditions'))
+  })
+}
