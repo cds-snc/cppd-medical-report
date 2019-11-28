0863c51238b981b6e5677d6fc93c5df9e19ab16e
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-28 15:17:15 -0500

    Add other box for symptoms uknown (#103)
    * add other box for symptoms uknown and refactor existing script to work for both
    
    * fix label
    
    * add better validation messages

--- routes/add_condition/js/add_condition.js
+++ routes/add_condition/js/add_condition.js
@@ -1,19 +1,24 @@
-const unknown = document.getElementById('unknown_details')
-const radios = document.getElementsByName('condition_outlook')
+function checkUnknowns(detailsId, radiosName, unknownValue) {
+  const unknown = document.getElementById(detailsId)
+  const radios = document.getElementsByName(radiosName)
 
-unknown.style.display = 'none'
+  unknown.style.display = 'none'
 
-for (var i = 0, len = radios.length; i < len; i++) {
-  if (radios[i].checked) {
-    if (radios[i].value === '4') {
-      unknown.style.display = 'block'
+  for (var i = 0, len = radios.length; i < len; i++) {
+    if (radios[i].checked) {
+      if (radios[i].value === unknownValue) {
+        unknown.style.display = 'block'
+      }
     }
-  }
-  radios[i].onclick = function() {
-    if (this.checked && this.value === '4') {
-      unknown.style.display = 'block'
-    } else {
-      unknown.style.display = 'none'
+    radios[i].onclick = function() {
+      if (this.checked && this.value === unknownValue) {
+        unknown.style.display = 'block'
+      } else {
+        unknown.style.display = 'none'
+      }
     }
   }
 }
+
+checkUnknowns('unknown_outlook_details', 'condition_outlook', '4')
+checkUnknowns('unknown_symptoms_occur_details', 'symptoms_occur', '3')
