7ff94b0f2a4596e3781d95afd8c3777876258c5a
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-28 10:18:09 -0500

    testing

--- routes/delete_condition/delete_condition.controller.js
+++ routes/delete_condition/delete_condition.controller.js
@@ -3,7 +3,7 @@ const { getSessionData } = require('./../../utils')
 module.exports = (app, route) => {
   route.draw(app).post((req, res) => {
     const data = getSessionData(req)
-    const index = req.params.id - 1 // array is zero-indexed
+    const index = req.params.id - 1 // array is zero-indexed test
 
     data.conditions.splice(index, 1)
 
