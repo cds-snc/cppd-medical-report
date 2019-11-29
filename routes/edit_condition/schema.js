a169d1730e63a3b4009f036a31c9384b270cd1ec
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-11-29 15:04:27 -0500

    Edit conditions (#99)
    * restyling conditions screen
    
    * bit of styling
    
    * add some margin
    
    * enable condition deletion
    
    * remove unused files
    
    * redirect if empty
    
    * fix a bit of styling
    
    * just getting started
    
    * enable condition edits
    
    * add edit js
    
    * copy over validation from add route

---
+++ routes/edit_condition/schema.js
@@ -0,0 +1,72 @@
+/* istanbul ignore file */
+
+const Schema = {
+  name_of_condition: {
+    isLength: {
+      errorMessage: 'errors.name_of_condition.length',
+      options: { min: 1 },
+    },
+  },
+  symptoms_began: {
+    isLength: {
+      errorMessage: 'errors.symptoms_began.length',
+      options: { min: 1 },
+    },
+  },
+  clinically_impair: {
+    isLength: {
+      errorMessage: 'errors.clinically_impair',
+      options: { min: 1 },
+    },
+  },
+  condition_outlook: {
+    isIn: {
+      errorMessage: 'errors.condition_outlook_required',
+      options: [['1', '2', '3', '4']],
+    },
+  },
+  condition_outlook_unknown: {
+    custom: {
+      options: (value, { req }) => {
+        const conditionOutlook = req.body.condition_outlook
+        if (conditionOutlook === '4') {
+          if (value === '') {
+            return false
+          }
+        }
+        return true
+      },
+      errorMessage: 'errors.condition_outlook_unknown',
+    },
+  },
+  condition_last: {
+    isIn: {
+      errorMessage: 'errors.condition_last_required',
+      options: [['1', '2']],
+    },
+  },
+  symptoms_occur: {
+    isIn: {
+      errorMessage: 'errors.symptoms_occur_required',
+      options: [['1', '2', '3']],
+    },
+  },
+  symptoms_occur_uknown: {
+    custom: {
+      options: (value, { req }) => {
+        const symptomsOccur = req.body.symptoms_occur
+        if (symptomsOccur === '3') {
+          if (value === '') {
+            return false
+          }
+        }
+        return true
+      },
+      errorMessage: 'errors.symptoms_occur_uknown_required',
+    },
+  },
+}
+
+module.exports = {
+  Schema,
+}
