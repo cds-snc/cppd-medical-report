5d5150ddc1588441daad5c09e17c4281819f5d90
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-12-04 09:55:35 -0500

    Add feature flag middleware (#111)
    * add featureFlag through middleware so it's available everywhere
    
    * remove featureFlag from controllers
    
    * use app.locals

--- routes/conditions/conditions.controller.js
+++ routes/conditions/conditions.controller.js
@@ -1,7 +1,6 @@
 /* istanbul ignore file */
 const { routeUtils, getSessionData } = require('./../../utils')
 const { Schema } = require('./schema.js')
-const featureFlag = require('./../../utils/featureFlags.helpers')
 
 module.exports = (app, route) => {
   const name = route.name
@@ -14,10 +13,7 @@ module.exports = (app, route) => {
       if (!data.conditions || data.conditions.length === 0) {
         res.redirect(res.locals.routePath('add_condition'))
       } else {
-        res.render(
-          name,
-          routeUtils.getViewData(req, { featureFlag: featureFlag }),
-        )
+        res.render(name, routeUtils.getViewData(req, {}))
       }
     })
     .post(route.applySchema(Schema), route.doRedirect())
